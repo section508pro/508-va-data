c2e26bbdea0289b9e850996a73b33adee41fa527
Scott Davis <scott@thirstyhead.com>
2023-08-03 18:04:37 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# 508-va-data
+Section 508 Data for VA
---
+++ json.js
@@ -0,0 +1,24 @@
+import Walker from 'walker';
+import fs from 'fs';
+
+buildJson('mobile');
+buildJson('desktop');
+
+function buildJson(type){
+    let dates = [];
+
+    Walker(`reports/${type}`)
+    .on('dir', (dir, stat) => {
+        const dirParts = dir.split('/');
+       if(dirParts.length == 3){
+            dates.push(dir);
+       }       
+    })
+    .on('end', ()=>{
+        try{
+            fs.writeFileSync(`reports/${type}/index.json`, JSON.stringify(dates));
+        }catch(err){
+            console.error(err);
+        }        
+    });
+}   
